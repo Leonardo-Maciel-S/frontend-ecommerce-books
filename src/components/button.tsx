d8cca861e0da76d3b73c4712eee6ebb62b8f8371
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-17 14:00:36 -0300

    feat: add htmlbuttonAtributes to Button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,11 @@
-import type { ReactNode } from "react";
+import type { HTMLAttributes } from "react";
 
-const Button = ({ children }: { children: ReactNode }) => {
+const Button = ({ children, ...props }: HTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
       type="button"
       className="group transition-all duration-200 p-3 cursor-pointer shadow-none hover:shadow-md hover:bg-private-secondary hover:border-transparent shadow-zinc-500 border border-gray-300 w-full rounded-xl "
+      {...props}
     >
       <span className="font-semibold text-gray-600 group-hover:text-white  transition-all duration-200">
         {children}
