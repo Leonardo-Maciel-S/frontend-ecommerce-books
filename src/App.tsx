4182c3e9524435badc1d942aef082067f1e9f71f
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-25 14:09:51 -0300

    feat: add alert about api deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import { Outlet } from "react-router";
 import HomeBar from "./components/home-bar/home-bar";
 import { ToastContainer } from "react-toastify";
 import useCheckIsLogged from "./hooks/user/use-check-is-logged";
+import { useEffect } from "react";
 
 function App() {
   useCheckIsLogged();
+
+  useEffect(() => {
+    window.alert(
+      'Devido a utilização do render no deploy da api, a mesma fica inativa depois de um tempo inativo, por esse motivo a primeira busca dos livros demora um pouco esperando a api subir novamente, após sumir a mensagem de "carregando" fica normal'
+    );
+  }, []);
 
   return (
     <div className="max-w-screen min-h-dvh border">
