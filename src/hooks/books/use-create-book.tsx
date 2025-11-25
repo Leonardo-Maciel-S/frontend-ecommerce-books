6838d63617a0233c1a8f01675941bc1036b0e2b4
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-25 08:29:18 -0300

    refactor: removed  unused import

--- src/hooks/books/use-create-book.tsx
+++ src/hooks/books/use-create-book.tsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import * as yup from "yup";
 import useGetUserAuth from "../user/use-get-user-auth";
 import { queryClient } from "@/main";
+import { useLogout } from "../user/use-logout";
 
 const createBookSchema = yup.object({
   title: yup
