06ceb1cc97b349e69b48f987932500a01082af6a
Leonardo Maciel <leonardomacielj@gmail.com>
2025-12-09 16:22:31 -0300

    feat: create hook to get cep

---
+++ src/hooks/address/get-cep.tsx
@@ -0,0 +1,39 @@
+import { cepService } from "@/services/cep/search";
+import { useMutation } from "@tanstack/react-query";
+import { useRef } from "react";
+import { toast } from "react-toastify";
+
+const useGetCep = () => {
+  const inputCep = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationKey: ["cep"],
+    mutationFn: async (cep: string) => {
+      const cleanCep = cep.replace(/\D/g, "");
+
+      if (!cleanCep || cleanCep.length !== 8) {
+        throw new Error("CEP Inválido");
+      }
+
+      try {
+        const cepInfo = await cepService.search(cep);
+
+        if (cepInfo) {
+          console.log(cepInfo);
+          toast.success("Cep encontrado");
+          return cepInfo;
+        }
+      } catch (error) {
+        toast.error("Cep não encontrado.");
+        throw new Error();
+      }
+    },
+  });
+
+  return {
+    ...mutation,
+    inputCep,
+  };
+};
+
+export default useGetCep;
