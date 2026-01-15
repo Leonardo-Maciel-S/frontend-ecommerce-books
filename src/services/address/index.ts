22fa00bceaa95c08827847c3267d2d0d50e0a577
Leonardo Maciel <leonardomacielj@gmail.com>
2026-01-14 23:22:39 -0300

    feat: add address service in index

--- src/services/address/index.ts
+++ src/services/address/index.ts
@@ -1,7 +1,11 @@
 import { createAddress } from "./create-address";
+import { editAddress } from "./edit-address";
+import { getAddressById } from "./get-address-by-id";
 import { getAllAddress } from "./get-all-address";
 
 export const addressService = {
   createAddress,
   getAllAddress,
+  getAddressById,
+  editAddress,
 };
