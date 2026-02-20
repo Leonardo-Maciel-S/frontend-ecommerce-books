5bdfe03905bb40a37a2ead107d5463ceb9036a44
Leonardo Maciel <leonardomacielj@gmail.com>
2026-02-20 14:41:54 -0300

    feat: show message when no comments

--- src/pages/BookDetails/comment/comment-list.tsx
+++ src/pages/BookDetails/comment/comment-list.tsx
@@ -1,16 +1,27 @@
 import type { Comment } from "@/@types/comment";
 import BookComment from "./comment";
+import ShowComponent from "@/components/show-component";
 
 interface CommentListProps {
-  bookComments: Comment[];
+  bookComments?: Comment[];
 }
 
 const CommentList = ({ bookComments }: CommentListProps) => {
+  const isEmptyList = bookComments?.length === 0;
+
   return (
     <div className="flex flex-col gap-4">
-      {bookComments.map((comment) => (
-        <BookComment key={comment.id} comment={comment} />
-      ))}
+      <ShowComponent when={isEmptyList}>
+        <p className="text-center text-lg p-4 font-bold italic">
+          Nenhum comentário cadastrado.
+        </p>
+      </ShowComponent>
+
+      <ShowComponent when={!isEmptyList}>
+        {bookComments?.map((comment) => (
+          <BookComment key={comment.id} comment={comment} />
+        ))}
+      </ShowComponent>
     </div>
   );
 };
