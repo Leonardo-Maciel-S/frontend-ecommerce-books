ad9651aec8ec1bb2dae418fffd0bd2e1eff5d43b
Leonardo Maciel <leonardomacielj@gmail.com>
2026-02-26 12:32:43 -0300

    feat: create service to edit comment

---
+++ src/services/comment/edit.ts
@@ -0,0 +1,24 @@
+import type { Comment, CommentBody } from "@/@types/comment";
+import { api } from "@/lib/axios";
+import type { AxiosError } from "axios";
+
+interface EditCommentParams {
+  id: string;
+  data: CommentBody;
+}
+
+export async function editComment({ id, data }: EditCommentParams) {
+  try {
+    const response = await api.patch<Comment>(`/comment/${id}`, data, {
+      withCredentials: true,
+    });
+
+    console.log(response);
+
+    if (response.data) {
+      return response.data;
+    }
+  } catch (error) {
+    throw new Error((error as AxiosError).message);
+  }
+}
--- src/services/comment/index.ts
+++ src/services/comment/index.ts
@@ -1,9 +1,11 @@
 import { createComment } from "./create";
 import { deleteComment } from "./delete";
+import { editComment } from "./edit";
 import { getAllCommentByBookId } from "./get-all-by-book-id";
 
 export const commentService = {
   createComment,
   getAllCommentByBookId,
   deleteComment,
+  editComment,
 };
