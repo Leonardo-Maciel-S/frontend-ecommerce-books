3401267c9072ef72d8d5aee23319e1190ed76a55
Leonardo Maciel <leonardomacielj@gmail.com>
2026-03-12 15:37:18 -0300

    style: refactoring register design

--- src/pages/login/component/register.tsx
+++ src/pages/login/component/register.tsx
@@ -1,7 +1,6 @@
 import ErrorMessage from "@/components/error-message";
 import ShowComponent from "@/components/show-component";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import {
   InputGroup,
   InputGroupAddon,
@@ -11,7 +10,7 @@ import {
 import useRegister from "@/hooks/user/use-register";
 
 import { Label } from "@radix-ui/react-label";
-import { Eye, EyeClosed } from "lucide-react";
+import { Eye, EyeClosed, Loader2, LockKeyhole, Mail, User } from "lucide-react";
 import { useState } from "react";
 
 const Register = ({ setSignIn }: { setSignIn: React.Dispatch<boolean> }) => {
@@ -28,58 +27,74 @@ const Register = ({ setSignIn }: { setSignIn: React.Dispatch<boolean> }) => {
   return (
     <form
       onSubmit={handleSubmit(submitRegister)}
-      className="flex flex-col gap-2 z-20 bg-white rounded-xl py-5 px-6 shadow-xl/50 min-w-[270px] md:w-[400px]"
+      className="flex flex-col gap-2 z-20 bg-white rounded-xl py-5 px-6  min-w-[270px] md:w-[400px] border border-zinc-200"
     >
-      <h2 className="text-center font-semibold font-primary  text-2xl leading-normal">
+      <h2 className="text-center font-extrabold text-3xl leading-normal">
         Criar conta
       </h2>
 
-      <div className="flex flex-col gap-2 mb-5">
-        <div className="flex flex-col gap-2  ">
-          <Label className="font-secondary text-md font-semibold">Nome:</Label>
-          <Input
-            {...register("name")}
-            type="text"
-            className="font-secondary md:text-md min-w-[100px]"
-            placeholder="Digite seu nome"
-          />
+      <div className="flex flex-col mb-5 gap-2">
+        <div className="flex flex-col gap-1 ">
+          <Label className="text-lg font-semibold text-zinc-500">Nome</Label>
+
+          <InputGroup className="h-12 bg-background">
+            <InputGroupInput
+              {...register("name")}
+              type="text"
+              className=" md:text-lg placeholder:text-zinc-400 text-zinc-600 font-medium  "
+              placeholder="Digite seu nome"
+            />
+            <InputGroupAddon>
+              <User className="text-zinc-400 text-4xl size-5" />
+            </InputGroupAddon>
+          </InputGroup>
+          <ShowComponent when={errors.name !== undefined}>
+            <ErrorMessage>{errors.name?.message}</ErrorMessage>
+          </ShowComponent>
+        </div>
+
+        <div className="flex flex-col gap-1 ">
+          <Label className="text-lg font-semibold text-zinc-500">Email</Label>
+          <InputGroup className="h-12 bg-background">
+            <InputGroupInput
+              {...register("email")}
+              type="text"
+              className=" md:text-lg placeholder:text-zinc-400 text-zinc-600 font-medium  "
+              placeholder="email@email.com"
+            />
+            <InputGroupAddon>
+              <Mail className="text-zinc-400 text-4xl size-5" />
+            </InputGroupAddon>
+          </InputGroup>
 
           <ShowComponent when={errors.email !== undefined}>
             <ErrorMessage>{errors.email?.message}</ErrorMessage>
           </ShowComponent>
         </div>
 
-        <div className="flex flex-col gap-2  ">
-          <Label className="font-secondary text-md font-semibold">Email:</Label>
-          <Input
-            {...register("email")}
-            type="text"
-            className="font-secondary md:text-md min-w-[100px]"
-            placeholder="email@email.com"
-          />
+        <div className="flex flex-col gap-2">
+          <Label className="text-lg font-semibold text-zinc-500">Senha</Label>
+          <InputGroup className="h-12 bg-background">
+            <InputGroupAddon>
+              <LockKeyhole className="text-zinc-400 text-4xl size-5" />
+            </InputGroupAddon>
 
-          <ShowComponent when={errors.email !== undefined}>
-            <ErrorMessage>{errors.email?.message}</ErrorMessage>
-          </ShowComponent>
-        </div>
-
-        <div className="flex flex-col gap-2 font-secondary">
-          <Label className=" font-secondary text-md font-semibold">
-            Senha:
-          </Label>
-          <InputGroup>
             <InputGroupInput
               {...register("password")}
               type={showPassword ? "text" : "password"}
-              className="font-secondary md:text-md min-w-[100px]"
-              placeholder="digite sua senha."
+              className=" md:text-lg text-zinc-600 font-medium "
+              placeholder="●●●●●●●●"
             />
             <InputGroupAddon align="inline-end">
               <InputGroupButton
                 className="cursor-pointer"
                 onClick={() => setShowPassword(!showPassword)}
               >
-                {showPassword ? <Eye /> : <EyeClosed />}
+                {showPassword ? (
+                  <Eye className="text-zinc-400 text-4xl size-5" />
+                ) : (
+                  <EyeClosed className="text-zinc-400 text-4xl size-5" />
+                )}
               </InputGroupButton>
             </InputGroupAddon>
           </InputGroup>
@@ -91,7 +106,7 @@ const Register = ({ setSignIn }: { setSignIn: React.Dispatch<boolean> }) => {
       </div>
 
       <Button disabled={isPending} className="cursor-pointer md:text-lg p-6">
-        Cadastrar
+        {isPending ? <Loader2 className="animate-spin" /> : "Cadastrar"}
       </Button>
     </form>
   );
