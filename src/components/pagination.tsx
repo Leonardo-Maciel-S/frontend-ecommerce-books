ae47181c4517402ef8efd8d51f42ee8368e2fcbe
Leonardo Maciel <leonardomacielj@gmail.com>
2026-03-28 15:00:43 -0300

    style: change color of page buttons

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -12,22 +12,38 @@ const Pagination = ({ actualPage, totalPages }: PaginationProps) => {
   const [_, setSearchParams] = useSearchParams();
 
   return (
-    <div className="flex gap-3 items-center ">
+    <div className="flex gap-3 items-center justify-center">
       <PrimaryButton
         disabled={actualPage <= 1}
         variant="outline"
         size="sm"
-        className="border-zinc-200 group "
+        className="border-zinc-200 group disabled:border-zinc-200/50"
         onClick={() => setSearchParams({ page: String(actualPage - 1) })}
       >
         <ChevronLeft
           size={20}
-          className="text-zinc-400 group-hover:text-primary group-disabled:text-zinc-400 "
+          className="text-zinc-400 group-hover:text-primary group-disabled:text-zinc-400/50 "
         />
       </PrimaryButton>
 
       <div className="flex items-center gap-2">
-        <PrimaryButton size="sm" className="rounded-xl font-primary py-3 px-4">
+        <ShowComponent when={actualPage - 1 > 0}>
+          <PrimaryButton
+            onClick={() => setSearchParams({ page: String(actualPage - 1) })}
+            variant="outline"
+            size="sm"
+            className="rounded-xl text-zinc-500 hover:text-primary font-primary py-3 px-4 border-primary/0 hover:border-inherit"
+          >
+            {actualPage - 1}
+          </PrimaryButton>
+        </ShowComponent>
+
+        <PrimaryButton
+          onClick={() => setSearchParams({ page: String(actualPage) })}
+          variant="primary"
+          size="sm"
+          className="rounded-xl font-primary py-3 px-4"
+        >
           {actualPage}
         </PrimaryButton>
 
@@ -36,25 +52,10 @@ const Pagination = ({ actualPage, totalPages }: PaginationProps) => {
             onClick={() => setSearchParams({ page: String(actualPage + 1) })}
             variant="outline"
             size="sm"
-            className="rounded-xl font-primary py-3 px-4"
+            className="rounded-xl text-zinc-500 hover:text-primary font-primary py-3 px-4 border-primary/0 hover:border-inherit "
           >
             {actualPage + 1}
           </PrimaryButton>
-        </ShowComponent>
-
-        <ShowComponent when={actualPage + 2 <= totalPages}>
-          <PrimaryButton
-            onClick={() => setSearchParams({ page: String(actualPage + 2) })}
-            variant="outline"
-            size="sm"
-            className="rounded-xl font-primary py-3 px-4"
-          >
-            {actualPage + 2}
-          </PrimaryButton>
-        </ShowComponent>
-
-        <ShowComponent when={actualPage + 3 <= totalPages}>
-          <span>...</span>
         </ShowComponent>
       </div>
 
@@ -62,12 +63,12 @@ const Pagination = ({ actualPage, totalPages }: PaginationProps) => {
         disabled={actualPage >= totalPages}
         variant="outline"
         size="sm"
-        className="border-zinc-200 group"
+        className="border-zinc-200 group disabled:border-zinc-200/50"
         onClick={() => setSearchParams({ page: String(actualPage + 1) })}
       >
         <ChevronRight
           size={20}
-          className="text-zinc-400 group-hover:text-primary group-disabled:text-zinc-400 "
+          className="text-zinc-400 group-hover:text-primary group-disabled:text-zinc-400/50 "
         />
       </PrimaryButton>
     </div>
