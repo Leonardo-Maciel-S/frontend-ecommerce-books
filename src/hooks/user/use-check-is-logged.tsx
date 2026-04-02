d93ceb28f10edbfc066124dd500e066ddd44938e
Leonardo Maciel <leonardomacielj@gmail.com>
2026-04-02 15:30:20 -0300

    fix: add react query in check logged

--- src/hooks/user/use-check-is-logged.tsx
+++ src/hooks/user/use-check-is-logged.tsx
@@ -1,17 +1,26 @@
 import { authService } from "@/services/auth";
 import type { AxiosError } from "axios";
 import useGetUserAuth from "./use-get-user-auth";
+import { useQuery } from "@tanstack/react-query";
 
 const useCheckIsLogged = async () => {
   const { removeUserAuth } = useGetUserAuth();
 
-  try {
-    await authService.isLogged();
-  } catch (e) {
-    if ((e as AxiosError).status === 401) {
-      removeUserAuth();
-    }
-  }
+  useQuery({
+    queryKey: ["check-logged"],
+    queryFn: async () => {
+      try {
+        await authService.isLogged();
+      } catch (e) {
+        if ((e as AxiosError).status === 401) {
+          removeUserAuth();
+        }
+      }
+
+      return null;
+    },
+    staleTime: 1000 * 60 * 5,
+  });
 };
 
 export default useCheckIsLogged;
