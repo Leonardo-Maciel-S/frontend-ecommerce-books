ac5feff7e5125282c85179cd33b5f97ad23c78b9
Leonardo Maciel <leonardomacielj@gmail.com>
2026-04-03 21:04:15 -0300

    feat: button disabled on loading

--- src/components/input-quantity.tsx
+++ src/components/input-quantity.tsx
@@ -4,18 +4,23 @@ interface InputQuantityProps {
   defaultQuantity: number;
   btnNextFn: () => void;
   btnPrevFn: () => void;
+  prevLoad?: boolean;
+  nextLoad?: boolean;
 }
 
 const InputQuantity = ({
   defaultQuantity = 1,
   btnNextFn,
   btnPrevFn,
+  prevLoad = false,
+  nextLoad = false,
 }: InputQuantityProps) => {
   return (
     <div className="bg-[#EEEBE9] rounded-xl overflow-hidden flex items-center py-1 w-28">
       <button
-        className="px-2 text-zinc-500 hover:text-primary cursor-pointer"
-        onClick={btnNextFn}
+        className="px-2 text-zinc-500 not-disabled:hover:text-primary cursor-pointer disabled:text-zinc-300"
+        onClick={btnPrevFn}
+        disabled={defaultQuantity <= 1 || prevLoad}
       >
         <Minus />
       </button>
@@ -23,8 +28,9 @@ const InputQuantity = ({
       <span className="flex-1 text-center font-bold">{defaultQuantity}</span>
 
       <button
-        className="px-2 text-zinc-500 hover:text-primary cursor-pointer"
-        onClick={btnPrevFn}
+        className="px-2 text-zinc-500 hover:text-primary cursor-pointer disabled:text-zinc-300"
+        onClick={btnNextFn}
+        disabled={nextLoad}
       >
         <Plus />
       </button>
