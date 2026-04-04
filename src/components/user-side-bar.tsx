57fbbe8105b603e139ec1be9b95dac861ba44f9c
Leonardo Maciel <leonardomacielj@gmail.com>
2026-04-04 12:33:19 -0300

    style: fix user bar padding

--- src/components/user-side-bar.tsx
+++ src/components/user-side-bar.tsx
@@ -3,7 +3,7 @@ import { useLogout } from "@/hooks/user/use-logout";
 import useGetUserAuth from "@/hooks/user/use-get-user-auth";
 import { Link } from "react-router";
 import { BookMarked, BookOpenText, MapPin, NotebookPen } from "lucide-react";
-import ProfileButton from "@/pages/profile/profile-button";
+import ProfileButton from "@/pages/profile/components/profile-button";
 
 const UserSideBar = () => {
   const { user, navigate } = useGetUserAuth();
@@ -17,12 +17,14 @@ const UserSideBar = () => {
   return (
     <div className="bg-white h-screen px-5 pb-8 flex justify-between flex-col">
       <div>
-        <Link to="/">
-          <button className="flex gap-2 items-center px-10 font-bold cursor-pointer font-secondary py-10  w-full">
-            <BookOpenText className="text-primary text-2xl" strokeWidth={3} />
-            <p className="text-2xl">Bookstore</p>
-          </button>
-        </Link>
+        <div className="pl-5 pr-16 py-10">
+          <Link to="/">
+            <button className="flex gap-2 items-center font-bold cursor-pointer font-secondary w-full">
+              <BookOpenText className="text-primary text-2xl" strokeWidth={3} />
+              <p className="text-2xl">Bookstore</p>
+            </button>
+          </Link>
+        </div>
 
         <div className="space-y-4  ">
           <ShowComponent when={!!user}>
